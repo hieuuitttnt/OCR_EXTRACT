96375356d81bc02e4cc51a989039ac829ffd1bfa
hieuuitttnt <114348338+hieuuitttnt@users.noreply.github.com>
2024-11-18 15:34:07 +0700

    Update misc.py

--- projects/SWINTS/swints/util/misc.py
+++ projects/SWINTS/swints/util/misc.py
@@ -452,14 +452,4 @@ def interpolate(input, size=None, scale_factor=None, mode="nearest", align_corne
     This will eventually be supported natively by PyTorch, and this
     class can go away.
     """
-    if float(torchvision.__version__[:3]) < 0.7:
-        if input.numel() > 0:
-            return torch.nn.functional.interpolate(
-                input, size, scale_factor, mode, align_corners
-            )
-
-        output_shape = _output_size(2, input, size, scale_factor)
-        output_shape = list(input.shape[:-2]) + list(output_shape)
-        return _new_empty_tensor(input, output_shape)
-    else:
-        return torchvision.ops.misc.interpolate(input, size, scale_factor, mode, align_corners)
+    return torchvision.ops.misc.interpolate(input, size, scale_factor, mode, align_corners)
